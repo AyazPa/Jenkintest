5e9778cbb8a6f65f51d192fedcec2459e7fbce92
AyazPasha <Ayaz9538433@gmail.com>
2020-03-10 09:45:16 +0530

    Adding close Browser

--- src/test/java/com/Testcases/Login.java
+++ src/test/java/com/Testcases/Login.java
@@ -3,17 +3,20 @@ package com.Testcases;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class Login
 {
+	@Test
 	public void doLogin() 
 	{
 		WebDriverManager.chromedriver().setup();
 		WebDriver driver = new ChromeDriver();
 		driver.get("http://www.way2automation.com/angularjs-protractor/banking/#/login");
 		driver.findElement(By.xpath("(//button[@class=\"btn btn-primary btn-lg\"])[2]")).click();
+		driver.close();
 	}
 		 
 }
